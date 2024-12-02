3d677c0092567529d75ab40a368dd33a3f6a985e
DanielKremes <danielkremes06@gmail.com>
2024-12-02 20:42:52 -0300

    add file 'script_02' inside folder 'python_ntroductory_notions'

---
+++ python_ntroductory_notions/script_02.py
@@ -0,0 +1,16 @@
+# Working with variables
+
+# Variable
+name_language_programming = ''
+age = 24
+is_active = True
+salary = 1600.0
+unployed = False
+
+print("What a programming language you are studying ? ")
+name_language_programming = input("")
+print(f"Welcome {name_language_programming}")
+print("")
+
+print(f"Your age {age}, doing college {is_active}, your curent salary {salary}, unployed {unployed}")
+
