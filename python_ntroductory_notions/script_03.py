ff4cf25a2159c3c1160c2184d2afca7225636dac
DanielKremes <danielkremes06@gmail.com>
2024-12-03 19:32:04 -0300

    add file 'script_03' inside folder 'python_ntroductory_notions'

---
+++ python_ntroductory_notions/script_03.py
@@ -0,0 +1,18 @@
+# Nath Operator
+print(1 + 1)
+print(1 - 1)
+print(1 * 1)
+print(1 / 1)
+print(1 // 1)
+print(1 % 1)
+print(1 ** 1)
+
+# Logical Operators
+print(1 > 1)
+print(1 < 1)
+print(1 >= 1)
+print(1 <= 1)
+print(1 != 1)
+print(1 == 1)
+
+
