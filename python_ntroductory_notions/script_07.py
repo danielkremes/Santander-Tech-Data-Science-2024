2e8ea4690df806eedf17201d6eca58b9b9393e08
DanielKremes <danielkremes06@gmail.com>
2024-12-04 21:46:32 -0300

    add script_07

---
+++ python_ntroductory_notions/script_07.py
@@ -0,0 +1,22 @@
+# import random as number_guess
+# number_random_system = number_guess.randint(0, 10)
+# numerber_user = 0
+# attempet = 0
+
+# for x in range(3):
+#     print("Enter the number !")
+#     numerber_user = int(input())
+    
+#     if numerber_user == number_random_system:
+#         print(f"number choose {numerber_user}, congratulations you're right!")
+#     else:
+#         print(f"number choose: {number_random_system}, sorry try again")
+#     attempet += 1
+#     print(f"Number attempet {attempet}")
+
+# sum = 0
+# for i in range(3):
+#     number = int(input("Number ?")) 
+#     sum += number
+# print(sum)
+
