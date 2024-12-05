3dba87a99f5f0d924aadcdde6b6b255646f726e9
DanielKremes <danielkremes06@gmail.com>
2024-12-04 21:02:35 -0300

    add file "script_05"

---
+++ python_ntroductory_notions/script_05.py
@@ -0,0 +1,38 @@
+# print("Enter your name")
+# name = input("")
+
+# print("Your age")
+# age = int(input(""))
+
+# if age >= 18:
+#     print("You're adult")
+# elif age > 0 and age < 18:
+#     print("you're degree")
+# else:
+#     print("Error try again")
+
+# average = 7.0
+
+# if average > 60:
+#     if average > 70:
+#         print("Congratulations you're approved")
+#     else:
+#         print("you're recuperation")
+# elif average < 50:
+#     print("Repproved") 
+# else:
+#     print("Error try again")
+    
+
+average = 7.0
+frequency = 70
+
+if average > 60:
+    if average > 70 and frequency >= 70:
+        print("Congratulations you're approved")
+    elif average > 70 and frequency >= 70:
+        print("you're recuperation")
+elif average < 50:
+    print("Repproved") 
+else:
+    print("Error try again")
